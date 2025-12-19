e518226dd99d46024ce0b1992d151b84c9cfd482
Ali Hussnain <2023cs613@student.uet.edu.pk>
2025-12-19 23:41:16 +0500

    finalized

--- Mess management/obj/Debug/net9.0/Mess management.AssemblyInfo.cs
+++ Mess management/obj/Debug/net9.0/Mess management.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mess management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+855a44951dd28eb4525ffdba2b71cf19b4de4266")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+244d30e3c523da02eaa67fce91a0f5cb1fc29055")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mess management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mess management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
